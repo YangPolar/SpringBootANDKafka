5aec9919bb97ad59355fc6c2cceafc37f7acf1bc
Yang-Kinto <liming.yang@kinto-mobility.biz>
2024-04-16 11:09:33 +0900

    JSON vs Kafka

--- springboot-kafka/src/main/java/com/example/springbootkafka/config/KafkaTopicConfig.java
+++ springboot-kafka/src/main/java/com/example/springbootkafka/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootkafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -9,8 +10,16 @@ import org.springframework.kafka.config.TopicBuilder;
  */
 @Configuration
 public class KafkaTopicConfig {
+    @Bean
     public NewTopic firstDemoTopic() {
         return TopicBuilder.name("firstDemo")
+                // .partitions(10)
+                .build();
+    }
+
+    @Bean
+    public NewTopic JsonTopic() {
+        return TopicBuilder.name("Demo_JSON")
                 // .partitions(10)
                 .build();
     }
